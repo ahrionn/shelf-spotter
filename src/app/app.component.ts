ac73676da7df2e8a646c080cda8673e5645b01ce
ahrionn <luam_azf@hotmail.com>
2024-10-16 20:07:55 -0300

    general updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'shelf-spotter';
+  constructor(
+    private updates: SwUpdate
+  ) {
+    if (this.updates.isEnabled) {
+      setInterval(() => {
+        this.updates.checkForUpdate().then((update) => {
+          if (update) {
+            const userConfirmed = confirm("Nova versão disponível! Deseja atualizar?");
+            if (userConfirmed) {
+              this.updates.activateUpdate().then(() => document.location.reload());
+            }
+          }
+        });
+      }, 300000);
+    }
+  }
 }
