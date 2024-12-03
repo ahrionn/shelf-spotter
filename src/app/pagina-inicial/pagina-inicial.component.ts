e6a0ccac8a31a8d19daccdada3e6972ad1a2675c
ahrionn <luam_azf@hotmail.com>
2024-12-03 16:37:37 -0300

    lógica de cache retirada

--- src/app/pagina-inicial/pagina-inicial.component.ts
+++ src/app/pagina-inicial/pagina-inicial.component.ts
@@ -21,39 +21,20 @@ export class PaginaInicialComponent {
   ) { }
 
   ngOnInit() {
-    let estoqueCache = localStorage.getItem('itensEstoque');
-    if (estoqueCache === null || estoqueCache === undefined || estoqueCache === '') {
-      this.isLoadingRequest = true;
-      this.http.get<any[]>(`${this.apiUrl}/api/listaEstoque`).subscribe({
-        next: (response) => {
-          this.isLoadingRequest = false;
-          let itensEstoque: any = response.sort((a, b) => a.nome.localeCompare(b.nome));
-          this.loadingInicialService.atualizarItensEstoque(itensEstoque);
-          itensEstoque = JSON.stringify(itensEstoque);
-          localStorage.setItem('itensEstoque', itensEstoque);
-        },
-        error: (error) => {
-          this.isLoadingRequest = false;
-          console.error('Erro ao buscar itens do estoque.', error);
-        }
-      });
-    } else {
-      let itensEstoque = estoqueCache !== null ? JSON.parse(estoqueCache) : [];
-      itensEstoque = itensEstoque.sort((a: { nome: string; }, b: { nome: any; }) => a.nome.localeCompare(b.nome));
-      this.loadingInicialService.atualizarItensEstoque(itensEstoque);
-
-      // Requisição inicial só pra "acordar"
-      this.http.get(`${this.apiUrl}/api/listaEstoque`).subscribe({
-        next: () => {
-          this.isLoadingRequest = false;
-          console.log('API acordada com sucesso');
-        },
-        error: (error) => {
-          this.isLoadingRequest = false;
-          console.error('Erro ao tentar acordar a API.', error);
-        }
-      });      
-    }
+    this.isLoadingRequest = true;
+    this.http.get<any[]>(`${this.apiUrl}/api/listaEstoque`).subscribe({
+      next: (response) => {
+        this.isLoadingRequest = false;
+        let itensEstoque: any = response.sort((a, b) => a.nome.localeCompare(b.nome));
+        this.loadingInicialService.atualizarItensEstoque(itensEstoque);
+        itensEstoque = JSON.stringify(itensEstoque);
+        localStorage.setItem('itensEstoque', itensEstoque);
+      },
+      error: (error) => {
+        this.isLoadingRequest = false;
+        console.error('Erro ao buscar itens do estoque.', error);
+      }
+    });
   }
 
   redirect(rota: string) {
